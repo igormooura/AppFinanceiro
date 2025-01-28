19adaff33b0b0e371cc428edc99fbf86f2ce23de
Ciro Moraes <133052976+ciromoraesr@users.noreply.github.com>
2025-01-27 21:56:35 -0300

    update porta backend

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ app.get("/", (req, res) => {
 });
 
 // Iniciando o servidor
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
