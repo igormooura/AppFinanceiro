706c54b2728ad0a94322d936dd03e1f41dfa4f5f
Igor Moura <igor.oliveira.moura@gmail.com>
2025-01-27 21:59:28 -0300

    fix: mudança da porta do backend

--- index.js
+++ index.js
@@ -46,8 +46,8 @@ app.get("/", (req, res) => {
   res.send("Bem-vindo à API de Criptomoedas!");
 });
 
-// Iniciando o servidor
-const PORT = process.env.PORT || 3000;
+// Inicializando o backend
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
