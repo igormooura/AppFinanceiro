17210c14311a01e3edba01464a153ff8cd93c75e
SouErick <ericksousasaraiva@gmail.com>
2025-02-17 01:03:34 -0300

    update client

--- model/AuthPerfil.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const AuthPerfilSchema = new mongoose.Schema({
-  email: { type: String, required: true, unique: true },
-  senha: { type: String, required: true },
-  usuarioId: { type: mongoose.Schema.Types.ObjectId, ref: "Usuario" }
-});
-
-module.exports = mongoose.model("AuthPerfil", AuthPerfilSchema);
--- model/Noticia.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const NoticiaSchemas = new mongoose.Schema({
-  titulo: String,
-  descr: String,
-  data: String,
-  tag: String
-});
-module.exports = mongoose.model("Noticia", NoticiaSchemas);
