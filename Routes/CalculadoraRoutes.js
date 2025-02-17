9d9a57a37991d46611c0fd7ab9ac99b0bf802169
Igor Moura <igor.oliveira.moura@gmail.com>
2025-02-17 09:53:08 -0300

    feat: mudança nas rotas

--- Routes/CalculadoraRoutes.js
+++ Routes/CalculadoraRoutes.js
@@ -1,15 +1,4 @@
 const express = require("express");
-<<<<<<< HEAD
-const { realizarConversao, getHistorico } = require("../controller/CalculadoraController");
-const router = express.Router();
-
-// Rota para realizar conversão
-router.post("/", realizarConversao);
-// Rota para buscar histórico de conversões
-router.get("/historico", getHistorico);
-module.exports = router;
-// 2 rotas
-=======
 const { realizarConversao, getHistoricoByUser, adicionarHistorico } = require("../controller/CalculadoraController");
 const router = express.Router();
 
@@ -18,4 +7,3 @@ router.get("/historico/:userId", getHistoricoByUser);
 router.post("/historico/:userId", adicionarHistorico);
 
 module.exports = router;
->>>>>>> cd4bd102040b77ec12d267c3155ec371acc8049e
--- controller/CalculadoraController.js
+++ controller/CalculadoraController.js
@@ -1,49 +1,4 @@
 const Conversao = require("../model/Calculadora");
-<<<<<<< HEAD
-
-exports.realizarConversao = async (req, res) => {
-  const { valor, moedaOrigem, moedaDestino } = req.body;
-  if (valor === undefined || typeof valor !== "number" || !moedaOrigem || !moedaDestino) {
-    return res.status(400).json({
-      error: "Parâmetros inválidos. Informe valor, moedaOrigem e moedaDestino.",
-    });
-  }
-  // Tabelas de conversão
-  const taxasConversao = {
-    EUR: { BRL: 5.4 },
-    USD: { BRL: 5.0 },
-    BRL: { EUR: 0.18, USD: 0.2 },
-  };
-  // Verificação de taxa de conversão
-  if (taxasConversao[moedaOrigem] && taxasConversao[moedaOrigem][moedaDestino]) {
-    const taxa = taxasConversao[moedaOrigem][moedaDestino];
-    const resultado = valor * taxa;
-    // Armazenar a conversão realizada no banco de dados
-    try {
-      const novaConversao = new Conversao({
-        valor,
-        moedaOrigem,
-        moedaDestino,
-        resultado,
-      });
-      await novaConversao.save();
-      // Retornar o resultado da conversão
-      res.json({
-        resultado: `${valor} ${moedaOrigem} = ${resultado} ${moedaDestino}`,
-      });
-    } catch (error) {
-      res.status(500).json({ error: "Erro ao salvar conversão no banco de dados." });
-    }
-  } else {
-    res.status(400).json({ error: "Conversão não suportada entre essas moedas" });
-  }
-};
-// Obter Historico das conversões
-exports.getHistorico = async (req, res) => {
-  try {
-    const historico = await Conversao.find(); // Busca todas as conversões no banco
-    res.status(200).json(historico); // Retorna o histórico em formato JSON
-=======
 const Auth = require("../model/AuthPerfil.js");
 const axios = require("axios");
 
@@ -97,14 +52,10 @@ exports.getHistoricoByUser = async (req, res) => {
   try {
     const historico = await Conversao.find({ user_id: userId });
     res.status(200).json(historico);
->>>>>>> cd4bd102040b77ec12d267c3155ec371acc8049e
   } catch (error) {
     console.error("Erro ao buscar histórico de conversões:", error);
     res.status(500).json({ error: "Erro ao buscar histórico de conversões." });
   }
-<<<<<<< HEAD
-};
-=======
 };
 
 exports.adicionarHistorico = async (req, res) => {
@@ -142,4 +93,3 @@ exports.adicionarHistorico = async (req, res) => {
     res.status(500).json({ error: "Erro ao adicionar histórico." });
   }
 };
->>>>>>> cd4bd102040b77ec12d267c3155ec371acc8049e
