94183b9b202894bbfecfe83d8b19681f19fcbb33
Igor Moura <igor.oliveira.moura@gmail.com>
2025-03-11 18:45:08 -0300

    refactor: translated pages to english

---
+++ client/src/pages/AboutUs/AboutUs.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import Sidebar from "../../components/SideBar/Sidebar";
+import Title from "../../components/Title/Title";
+import PageName from "../../components/PageName/PageName";
+import useAuth from "../../hooks/useAuth";
+
+function SobreNos() {
+  useAuth();
+
+
+  return (
+    <div className="lg:flex h-screen w-full">
+      <Sidebar />
+
+      <div className="flex-1 bg-gradient-to-b from-[#C0F0B1] h-full to-white p-5 relative">
+
+        <Title/>
+
+        <PageName name="About us"/>
+
+        <div
+          className="absolute left-1/2 transform -translate-x-1/2 lg:w-[600px] w-[90%]"
+          style={{
+            top: "40%",
+            
+          }}
+        >
+          <div className="bg-transparent rounded-lg shadow-lg p-5 border-2 border-gray-300 flex flex-col justify-center items-center">
+            <p className="text-[#6A7D64] font-montserrat-negrito text-[20px] mb-2">
+              <strong>Erick Sousa Saraiva</strong> - Ericksousasaraiva@gmail.com
+            </p>
+            <p className="text-[#6A7D64] font-montserrat-negrito text-[20px] mb-2">
+              <strong>Igor Oliveira Moura</strong> - igor.oliveira.moura@gmail.com
+            </p>
+            <p className="text-[#6A7D64] font-montserrat-negrito text-[20px] mb-5">
+              <strong>Ciro Rocha Moraes</strong> - ciromoraes.r@gmail.com
+            </p>
+
+      
+            <div className="flex justify-center">
+              <img
+                src="/static/ifb_simbol.png"
+                alt="Instituto Federal"
+                className="w-[150px]"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default SobreNos;
---
+++ client/src/pages/LoginPage/Login.jsx
@@ -0,0 +1,138 @@
+import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import EmailInput from "../../components/Inputs/EmailInput/EmailInput";
+import PasswordField from "../../components/Inputs/PasswordInput/Password";
+
+const LoginPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const [rememberMe, setRememberMe] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const handleRememberMe = () => {
+    setRememberMe((prev) => !prev);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (email.trim() === "") {
+      setError("Por favor, insira um e-mail válido.");
+      return;
+    }
+
+    setIsLoading(true);
+
+    try {
+      const response = await axios.post(`${process.env.REACT_APP_API_LINK}/auth/login`, {
+        email: email,
+        senha: password,
+      });
+
+      if (response.data.token) {
+        localStorage.setItem("token", response.data.token);
+        if (rememberMe) {
+          localStorage.setItem("rememberMe", true);
+        } else {
+          localStorage.removeItem("rememberMe");
+        }
+        navigate(`/grafico`);
+        
+      } else {
+        setError("Erro ao fazer login. Tente novamente.");
+      }
+    } catch (err) {
+      console.error(err);
+      setError("E-mail ou senha inválidos.");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <div className="flex">
+      <div
+        className="hidden lg:flex lg:w-2/3 h-screen items-center justify-center bg-green-500"
+        style={{
+          backgroundImage: "url('/static/bitcoin.jpg')",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+      >
+        <div className="w-full h-full bg-green-400/95 mx-auto my-auto flex items-center">
+          <img className="h-[90%] flex justify-center mx-auto" src="static/nome_logo.png"></img>
+        </div>
+      </div>
+      <nav className="bg-gradient-to-b lg:flex flex-col from-green-500 to-green-700/40 w-full lg:w-1/3 h-screen">
+        <img src="static/nome_logo.png" className="lg:hidden w-[250px] pt-10 -mt-20 mx-auto h-[35%]"></img>
+        <form onSubmit={handleSubmit} className="p-6 rounded mx-auto my-auto w-[80%]">
+          <h2 className="text-2xl mb-8 text-start text-white font-bold">
+            Welcome!
+          </h2>
+  
+          {error && <p className="text-red-500 text-sm mb-2">{error}</p>}
+  
+          <div className="mb-4">
+            <EmailInput email={email} setEmail={setEmail} placeholder={"Enter your email"} />
+          </div>
+  
+          <div className="mb-4">
+            <PasswordField
+              password={password}
+              setPassword={setPassword}
+              showPassword={showPassword}
+              setShowPassword={setShowPassword}
+              placeholder={"Enter your password"}
+            />
+          </div>
+  
+          <div className="flex mb-7">
+            <div className="flex mr-auto justify-start items-center">
+              <div
+                className={`rounded-3xl w-[60px] flex items-center h-[30px] cursor-pointer ${
+                  rememberMe ? "bg-green-400" : "bg-gray-200"
+                } transition-colors duration-300`}
+                onClick={handleRememberMe}
+              >
+                <div
+                  className={`bg-white h-6 w-6 rounded-full shadow-md transform ${
+                    rememberMe ? "translate-x-7 bg-green-900" : "translate-x-1"
+                  } transition-transform duration-300`}
+                ></div>
+              </div>
+              <p className="ml-4 text-white font-semibold">Remember me</p>
+            </div>
+  
+            <div className="ml-auto justify-end text-end text-sm flex mr-1">
+              <a href="/esquecer" className="text-blue-600 font-semibold">
+                Forgot password?
+              </a>
+            </div>
+          </div>
+  
+          <button
+            type="submit"
+            className="w-full bg-blue-500 text-white text-2xl font-bold shadow-xl p-2 rounded-lg hover:bg-blue-600 flex items-center justify-center"
+            disabled={isLoading}
+          >
+            {isLoading ? (
+              <div className="animate-spin rounded-full h-6 w-6 border-b-2 border-white"></div>
+            ) : (
+              "Login"
+            )}
+          </button>
+          <hr className="w-full h-[0.1px] bg-white mt-14"></hr>
+          <p className="text-white text-xl mx-auto items-center justify-center flex mt-10">
+            Don't have an account?&nbsp;
+            <a href="/register" className="text-blue-500"> Sign up</a>
+          </p>
+        </form>
+      </nav>
+    </div>
+  );
+};
+
+export default LoginPage;
